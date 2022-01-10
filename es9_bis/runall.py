e1be5f4422a1ca1b49f8ea79ab2cf332ae1fa645
evilla <emanuele.villa@cern.ch>
2022-01-10 10:01:30 +0100

    Final commit

--- es9_bis/runall.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import subprocess
-import shutil
-
-for sim in ["s1"]: #c1 and c2 done s2 
-    os.system('cp input.%s input.dat' %sim)
-    os.system('make')
-    os.system('cp *.out %s' %sim)
